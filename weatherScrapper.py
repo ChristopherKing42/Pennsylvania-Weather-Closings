1246204a30b534cca8003a816afb6381bd535ce3
Christopher King <G.nius.ck@gmail.com>
2015-01-24 12:37:55 -0500

    Added Comments

--- weatherScrapper.py
+++ weatherScrapper.py
@@ -18,25 +18,32 @@ from urllib import urlopen
 import re
 from itertools import chain
 
-javascript = re.compile(r"ibsys.htvClosings.init\(({.*?})\)")
+javascript = re.compile(r"ibsys.htvClosings.init\(({.*?})\)") #The closings are embeded in a javascript function. This will extract the dictionary to group 1.
 wgalAddress = "http://www.wgal.com/weather/closings"
-def closingDictionary(updateCache=False, cache="closings.html"):
-    '''Returns a dictionary containing information about closed schools and bussinesses.'''
-    if not cache: updateCache = True
-    if updateCache:
-        wgalSite = urlopen(wgalAddress).read()
-        if cache:
-            with open(cache, 'w') as cacheFile:
-                cacheFile.write(wgalSite)
-    else:
+def closingDictionary(useCache=True, cache="closings.html"):
+    """Returns a dictionary containing information about closed schools and bussinesses.
+
+    Keyword Arguments:
+
+    useCache -- If true function will use cache instead of going online (default True)
+    cache    -- The file that a cache of the website will be stored in. If none or the file does not exist, useCache will be changed to True (default \"closing.html\")
+    """
+    if not cache: useCache = False
+    if useCache:
         try:
             with open(cache, 'r') as cacheFile:
                 wgalSite = cacheFile.read()
         except IOError:
-            return closingDictionary(updateCache=True, cache=cache)
+            return closingDictionary(updateCache=False, cache=cache)
+    else:
+        wgalSite = urlopen(wgalAddress).read()
+        if cache:
+            with open(cache, 'w') as cacheFile:
+                cacheFile.write(wgalSite)
 
     wgal = soup(wgalSite, 'lxml')
     script = wgal.find(text = javascript)
-    rawData = eval(re.search(javascript, script).group(1))
-    organizedDict = {place.pop('name'): place for place in chain(*(letter['institutions'] for letter in rawData.values() if type(letter) == dict))}
+    rawData = eval(re.search(javascript, script).group(1)) #The parsed data is a valid python dictionary
+    organizedDict = {place.pop('name'): place for place in #Change the name into the key
+                     chain(*(letter['institutions'] for letter in rawData.values() if type(letter) == dict))} #The raw data is organized by letter. Extract "institutions" from each and chain them together.
     return organizedDict
